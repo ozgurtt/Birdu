016275bf5957c7137cc088fd969133d5f48e9559
jtronlabs <jtronlabs@gmail.com>
2016-04-03 20:09:21 -0400

    tweaking player movement

--- dist/js/game.js
+++ dist/js/game.js
@@ -47,6 +47,7 @@ var Protagonist = function(game, x, y, frame) {
   Phaser.Sprite.call(this, game, x, y, 'b-1', frame);
 
   this.anchor.setTo(0.5, 0.5);
+  this.scale.setTo(.5,.5);
 
   // add and play animations
   this.animations.add('idling', [0,1,2,3], flap_delay, true);
@@ -70,44 +71,64 @@ Protagonist.prototype = Object.create(Phaser.Sprite.prototype);
 Protagonist.prototype.constructor = Protagonist;
 
 Protagonist.prototype.update = function() {
-  var moving_horizontally = true;
-  this.animations.getAnimation('idling').delay = flap_delay / 2;
-  this.game.physics.arcade.gravity.y = 0;
-
-  //HORIZONTAL MOVEMENT
-  if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
-  {
-      this.body.velocity.y = -movement_speed;
-      this.angle = -15;
-  }
-  else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-  {
-      this.body.velocity.y = movement_speed;
-      this.angle = 15;
-  }else{
-    moving_horizontally = false
-  }
-  //VERTICAL MOVEMENT
-  if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-  {
-      this.body.velocity.x = movement_speed;
-      this.angle = 15;
-  }
-  else if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-  {
-      this.body.velocity.x = -movement_speed;
-      this.angle = -15;
-  }
-  //NO MOVEMENT
-  else if(!moving_horizontally)
-  {
-    this.game.physics.arcade.gravity.y = 90;
-    this.animations.getAnimation('idling').delay = flap_delay;
-    this.angle = 0;
-  }
-
+  handleMovement(this);
 
 };
+
+function handleMovement(player){
+  var moving_vertically = true;
+  player.animations.getAnimation('idling').delay = flap_delay / 2;
+
+  //VERTICAL MOVEMENT
+  if (player.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+  {
+    player.game.physics.arcade.gravity.y = 0;
+    player.body.velocity.y = -movement_speed;
+    if(player.scale.x > 0){ //sprite is facing right
+      player.angle = -15;
+    }else{//sprite is facing left
+      player.angle = 15;
+    }
+  }
+  else if (player.game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+  {
+    player.body.velocity.y = movement_speed;
+    if(player.scale.x > 0){ //sprite is facing right
+      player.angle = 15;
+    }else{//sprite is facing left
+      player.angle = -15;
+    }
+  }else{
+    moving_vertically = false
+  }
+  //HORIZONTAL MOVEMENT
+  if (player.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+  {
+    player.game.physics.arcade.gravity.y = 0;
+    player.scale.x = Math.abs(player.scale.x); //face sprite right
+    player.body.velocity.x = movement_speed;
+    player.angle = 15;
+  }
+  else if (player.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+  {
+    player.game.physics.arcade.gravity.y = 0;
+    player.scale.x = -1 * Math.abs(player.scale.x);//face sprite left
+    player.body.velocity.x = -movement_speed;
+    player.angle = -15;
+  }
+  //NO MOVEMENT
+  else if(!moving_vertically)
+  {
+    player.game.physics.arcade.gravity.y = 90;
+    player.animations.getAnimation('idling').delay = flap_delay;
+    player.angle = 0;
+  }
+
+}
+
+function getProtagonistArea(){
+  return this.height * this.width;
+}
 
 module.exports = Protagonist;
 
