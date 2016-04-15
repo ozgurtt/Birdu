830458e7004e573c7bd4db3ccfed54e342724c1b
jtronlabs <jtronlabs@gmail.com>
2016-04-15 03:05:54 -0400

    onPause in Android is ACTUALY WORKING now. I accidentally edited the previous commit after I got it working and broke it, fixed it here. Also renamed a few files to be more verbose

--- dist/assets/utilize_cordova_api.js
+++ dist/assets/utilize_cordova_api.js
@@ -1,10 +1,16 @@
 //Cordova specific functions
 
-
+/*
+IDK why, but placing this function call in window.onload or document.onload fails to fire it off,
+as does setting the call in the html (which gives a Content Security Policy error).
+So just call it here, whatever it works in Cordova
+*/
+gameContainerLoaded();
 
 // Wait for device API libraries to load
 //
-function onLoad() {
+function gameContainerLoaded() {
+  console.log("DOCUMENT LOADED")
   document.addEventListener("deviceready", onDeviceReady, false);
 }
 
